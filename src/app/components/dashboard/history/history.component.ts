5cd049790d28bd90236b5f7e49526f87badcd7fb
Siddharth Nahar <siddharth.nahar@techextensor.com>
2023-05-06 00:38:19 +0530

    transaction history pie chart changes

--- src/app/components/dashboard/history/history.component.ts
+++ src/app/components/dashboard/history/history.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ServiceDataService } from 'src/app/services/service-data.service';
 import { TransactionsService } from 'src/app/services/transactions.service';
-import {Chart, registerables} from 'node_modules/chart.js';
+import { Chart, registerables } from 'node_modules/chart.js';
 import { HttpErrorResponse } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import * as Notiflix from 'notiflix';
@@ -14,36 +14,36 @@ Chart.register(...registerables);
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.css']
 })
-export class HistoryComponent implements OnInit{
+export class HistoryComponent implements OnInit {
 
-  myId = JSON.parse(localStorage.getItem('userData')|| '{}');
+  myId = JSON.parse(localStorage.getItem('userData') || '{}');
 
-  historyData:any[] = [];
-  summaryData:any[] = [];
-  loading:Boolean = false;
+  historyData: any[] = [];
+  summaryData: any[] = [];
+  loading: Boolean = false;
 
-  chartdata:any;
+  chartdata: any;
 
-   labeldata:any[]=[];
-   realdata:any[]=[];
-   colordata:any[]=[];
+  labeldata: any[] = [];
+  realdata: any[] = [];
+  colordata: any[] = [];
 
   activePage: any = 1;
 
-  total_count : any;
+  total_count: any;
 
   totalRecord: number = 0;
   pagination: number = 1;
   pageSize: number = 10;
   totalPages: number = 1;
   showSpinner: Boolean = false;
-  paginationStatementData:any [] = [];
-  colordatas: any[] =['green', 'red'];
-  myLocalDatails = JSON.parse(localStorage.getItem('userData')|| '{}');
+  paginationStatementData: any[] = [];
+  colordatas: any[] = ['green', 'red'];
+  myLocalDatails = JSON.parse(localStorage.getItem('userData') || '{}');
 
   constructor(private _dataService: ServiceDataService,
     private _transactService: TransactionsService,
-    private _router: Router) {}
+    private _router: Router) { }
 
 
   ngOnInit(): void {
@@ -54,30 +54,30 @@ export class HistoryComponent implements OnInit{
   }
 
 
-  historyStatement(){
+  historyStatement() {
     this.showSpinner = true
-    this._dataService.getUserTranHistory(this.pagination, this.pageSize, this.myId._id).subscribe((res: any) =>{
-      if(res.msg == "200"){
+    this._dataService.getUserTranHistory(this.pagination, this.pageSize, this.myId._id).subscribe((res: any) => {
+      if (res.msg == "200") {
         this.historyData = res.data;
         this.totalRecord = res.total_record;
-        this.totalPages = window.Math.ceil(this.totalRecord/this.pageSize);
+        this.totalPages = window.Math.ceil(this.totalRecord / this.pageSize);
         // console.log(this.totalRecord);
         this.showSpinner = false
       }
-      else if(res.message === 'jwt malformed') {
+      else if (res.message === 'jwt malformed') {
         console.log("JWT Malformed error", res.message)
-          Notiflix.Notify.failure('Access denied!, Login to continue', {
-            width: '400px',
-            showOnlyTheLastOne: true,
-            position: 'right-bottom',
-            fontSize: '18px',
-          });
-          this.showSpinner = false
+        Notiflix.Notify.failure('Access denied!, Login to continue', {
+          width: '400px',
+          showOnlyTheLastOne: true,
+          position: 'right-bottom',
+          fontSize: '18px',
+        });
+        this.showSpinner = false
 
       }
     }, err => {
-      if(err instanceof HttpErrorResponse){
-        if(err.status === 401){
+      if (err instanceof HttpErrorResponse) {
+        if (err.status === 401) {
           // console.log
           console.log("Page error", err.message)
           Notiflix.Notify.failure('Access denied!, Login to continue', {
@@ -89,7 +89,7 @@ export class HistoryComponent implements OnInit{
           this.showSpinner = false
         }
       }
-      else if(err.status === 500){
+      else if (err.status === 500) {
         Notiflix.Notify.failure('System!, Login to continue', {
           width: '400px',
           showOnlyTheLastOne: true,
@@ -100,13 +100,13 @@ export class HistoryComponent implements OnInit{
     })
   }
 
-  reloadTable(){
-    this._dataService.getUserTranHistory(this.pagination, this.pageSize, this.myId._id).subscribe((res: any) =>{
+  reloadTable() {
+    this._dataService.getUserTranHistory(this.pagination, this.pageSize, this.myId._id).subscribe((res: any) => {
       this.historyData = res.data;
       this.totalRecord = res.total_record;
-      this.totalPages = window.Math.ceil(this.totalRecord/this.pageSize);
-     // console.log(this.totalRecord);
-     });
+      this.totalPages = window.Math.ceil(this.totalRecord / this.pageSize);
+      // console.log(this.totalRecord);
+    });
   }
 
   renderPage(event: number) {
@@ -116,53 +116,59 @@ export class HistoryComponent implements OnInit{
 
   //get user summary of transaction and sum them out
   fetchAccountSummary() {
-    this._dataService.getUserAcctSummary(this.myId._id).subscribe(res =>{
+    this._dataService.getUserAcctSummary(this.myId._id).subscribe(res => {
       this.summaryData = res;
-      });
+    });
   }
 
-  RenderChart(labeldata:any, maindata:any, colordata:any, type:any, id:any){
+  RenderChart(labeldata: any, maindata: any, colordata: any, type: any, id: any) {
     const myChart = new Chart(id, {
       type: type,
       data: {
-          datasets: [{
-              label: 'Record Statistic',
-              data: maindata,
-              backgroundColor: [
-                'green',
-                'red',
-                ],
-              borderColor: ['#D2D2D2'],
-              borderWidth: 1,
-              borderRadius: 5,
-          },
+        labels: ['Credit', 'Debit'],
+        datasets: [{
+          label: 'Amount',
+          data: maindata,
+          backgroundColor: colordata,
+          borderColor: ['#D2D2D2'],
+          borderWidth: 1,
+          borderRadius: 5,
+        },
         ],
       },
       options: {
-          scales: {
-              y: {
-                  beginAtZero: true
-              }
-          },
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        },
       }
-  });
+    });
   }
 
-  getFinanceChart(){
-    this._dataService.financeChartReport(this.myLocalDatails._id).subscribe(res =>{
+  getFinanceChart() {
+    this._dataService.financeChartReport(this.myLocalDatails._id).subscribe(res => {
+      debugger
       this.chartdata = res.data;
       //console.log("chart details", res.data)
-      if(this.chartdata!=null){
-        for(let i=0; i<this.chartdata.length; i++){
+      if (this.chartdata != null) {
+        for (let i = 0; i < this.chartdata.length; i++) {
           //console.log(this.chartdata[i]);
-          this.labeldata.push(this.chartdata[i]._id)
-          this.realdata.push(this.chartdata[i].amount)
-          this.colordata.push(this.chartdata[i].colordatas)
+          if (this.chartdata[i]._id == "Credit") {
+            this.labeldata.push(this.chartdata[i]._id)
+            this.realdata.push(this.chartdata[i].amount)
+            this.colordata.push('green')
+          } else {
+            this.labeldata.push(this.chartdata[i]._id)
+            this.realdata.push(this.chartdata[i].amount)
+            this.colordata.push(this.chartdata[i].colordatas)
+          }
+
         }
-        this.RenderChart(this.labeldata, this.realdata, this.colordata, 'bar', 'barchart');
+        // this.RenderChart(this.labeldata, this.realdata, this.colordata, 'bar', 'barchart');
         this.RenderChart(this.labeldata, this.realdata, this.colordatas, 'pie', 'piechart');
-        this.RenderChart(this.labeldata, this.realdata, this.colordata, 'line', 'dochart');
-        }
+        // this.RenderChart(this.labeldata, this.realdata, this.colordata, 'line', 'dochart');
+      }
     });
   }
 
